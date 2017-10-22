419841a40c5d73735ac617477053461f4a466780
Kingdon Barrett <kbarret8@nd.edu>
2017-10-22 13:23:43 -0400

    my_dashboard (pending branch)

---
+++ app/controllers/my_dashboard_controller.rb
@@ -0,0 +1,16 @@
+class MyDashboardController < ApplicationController
+  attr_accessor :api
+  attr_accessor :project
+  before_action :connect_pivotal_api
+
+  def index
+    binding.pry
+  end
+
+  private
+  def connect_pivotal_api
+    self.api = TrackerApi::Client.new(token: ENV.fetch('PIVOTAL_API_KEY'))
+    project_id = ENV.fetch('PIVOTAL_PROJECT')
+    self.project = api.project(project_id).stories(fields: ':default,tasks')  
+  end
+end
