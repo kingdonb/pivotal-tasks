f4e86537180e5a767a8265f7e1464881ae864536
Kingdon Barrett <kbarret8@nd.edu>
2017-11-18 14:00:20 -0500

    get stories (active or not)

--- app/controllers/my_dashboard_controller.rb
+++ app/controllers/my_dashboard_controller.rb
@@ -1,6 +1,7 @@
 class MyDashboardController < ApplicationController
   attr_accessor :api
   attr_accessor :project
+  attr_accessor :stories
   before_action :connect_pivotal_api
 
   def index
@@ -11,6 +12,8 @@ class MyDashboardController < ApplicationController
   def connect_pivotal_api
     self.api = TrackerApi::Client.new(token: ENV.fetch('PIVOTAL_API_KEY'))
     project_id = ENV.fetch('PIVOTAL_PROJECT')
-    self.project = api.project(project_id).stories(fields: ':default,tasks')  
+    self.project = api.project(project_id)
+    self.stories = project.stories(fields: ':default,tasks')
+    #self.open_tasks = stories.
   end
 end
