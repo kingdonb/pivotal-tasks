e11e5ec04a7232e571e759bf5dc6720cc5ad864b
Kingdon Barrett <kbarret8@nd.edu>
2017-11-19 11:44:44 -0500

    implement "ping" health check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # before_filter :authenticate, except: :ping
+
+  def ping
+    show_response("result", "success", :ok)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'my_dashboard/index'
 
+  get '/ping', to: 'application#ping', defaults: { :format => 'json' }
+
   resources :dashboards
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
