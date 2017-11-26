e13ae8c1216adf78eb9e296448ccbd16fec40a57
Kingdon Barrett <kbarret8@nd.edu>
2017-11-26 15:27:54 -0500

    render text: was deprecated and removed in rails 5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,33 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  # before_filter :authenticate, except: :ping
+  before_action :authenticate, except: :ping
+  #before_action :user_login
+
+  def authenticate
+    #render status: 401, html: "<p>redirect to CAS</p>".html_safe unless request.session.key?("cas")
+    render status: 401, plain: "" unless request.session.key?("cas")
+  end
+
+
+  def user_login
+    if session['cas']['user'].nil?
+      login_err("Your single sign on authentication could not be verified.")
+      return false
+    end
+    session['user_id'] = session['cas']['user'].upcase
+    #rval = check_access_and_setup_session(session['user_id']) if session['user_roles'].nil?
+    #User.find_by_netid(session['user_id']).try(:activity!)
+    #return rval
+
+  #rescue => e
+  #  login_err(<<-eos)
+  #    An error has occurred in verifying your user access to this application.
+  #    Verify that all appropriate web services are available.
+  #    class: #{e.class.name} : #{e.message}
+  #  eos
+  #  return false
+  end
 
   def ping
     show_response("result", "success", :ok)
